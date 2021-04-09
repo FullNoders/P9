5bbb539022c74d013efebe6ffad2551e03bc6111
Jorge Arrojo <arrojorge@uoc.edu>
2021-04-09 20:43:22 +0200

    css routering

--- router.js
+++ router.js
@@ -19,15 +19,16 @@ function renderTemplate(template, res) {
     //header
     const header = getTemplate('header');
     // CSS
-    const css_cover = fs.readFileSync(`${__dirname}/public/css/cover.css`, 'utf-8');
-    const css_styles = fs.readFileSync(`${__dirname}/public/css/style.css`, 'utf-8');
-    const css = '<style>' + css_styles + '</style>' + '<style>' + css_cover + '</style>';
+    //const css_cover = fs.readFileSync(`${__dirname}/public/css/cover.css`, 'utf-8');
+    //const css_styles = fs.readFileSync(`${__dirname}/public/css/style.css`, 'utf-8');
+    //const css = '<style>' + css_styles + '</style>' + '<style>' + css_cover + '</style>';
     //container
     const container = getTemplate(template);
     //footer
     const footer = getTemplate('footer');
     //Unimos todas las piezas html
-    let fullContent = header + css + container + footer;
+    //let fullContent = header + css + container + footer;
+    let fullContent = header + container + footer;
     //renderizamos el contenido completo
     res.end(fullContent);
 }
@@ -45,6 +46,28 @@ function routerUrl(path, res){
             break;
         case '/room':
             renderTemplate('room',res);
+            break;
+        case '/public/css/style.css':
+            const styles = fs.readFile(`${__dirname}/public/css/style.css`,'utf8',(err,data)=>{
+                if(err){
+                    res.end('Error');
+                }else{
+                    res.writeHead(200,{'Content-Type':'text/css'});
+                    res.write(data);
+                    res.end();
+                }
+            });
+            break;
+        case '/public/css/cover.css':
+            const cover = fs.readFile(`${__dirname}/public/css/cover.css`,'utf8',(err,data)=>{
+                if(err){
+                    res.end('Error');
+                }else{
+                    res.writeHead(200,{'Content-Type':'text/css'});
+                    res.write(data);
+                    res.end();
+                }
+            });
             break;
         default:
             res.writeHead(404);
