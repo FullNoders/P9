e12b0b69b40b65f2cd12183103a3220b674939a0
Jorge Arrojo <arrojorge@uoc.edu>
2021-04-22 19:44:34 +0200

    Starting with express

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
-import { routerUrl } from './router.js';
+/* import { routerUrl } from './router.js';
 import http from 'http';
-import url from 'url';
+import url from 'url'; */
 
-const hostname = '127.0.0.1';
+import express from 'express';
+//const express = require('express');
+const app = express();
 const port = 1338;
 
-const server = http.createServer((req, res) => {
-    const path = req.url;
 
-    routerUrl(path,res);
-    
+app.get('/',(req,res)=>{
+  res.type('text/plain');
+  res.send('Esto es la home');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+//500
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server error');
+});
+
+
+//404
+app.use((req,res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+
+
+
+
+//const hostname = '127.0.0.1';
+
+// const server = http.createServer((req, res) => {
+//     const path = req.url;
+
+//     //routerUrl(path,res);
+    
+// });
+
+app.listen(port, () => {
+  console.log(`Express running at http://localhost:${port}/`);
 });
 
