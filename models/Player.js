4e24060d7de514d6d06e4c1a2985958ea62acdd2
euoc <ebresba@uoc.edu>
2021-05-09 23:20:15 +0200

    models

---
+++ models/Player.js
@@ -0,0 +1,61 @@
+function Player(id, name, avatar) {
+    this.id = id;
+    this.name = name;
+    this.avatar = avatar;
+    this.points = 0;
+    this.percentageWin = 0;
+    this.ready = false;
+    this.position = null;
+
+    this.getId = function() {
+        return this.id;
+    }
+    this.setId = function(id) {
+        this.id = id;
+    }
+
+    this.getName = function() {
+        return this.name;
+    }
+    this.setName = function(name) {
+        this.name = name;
+    }
+
+    this.getAvatar = function() {
+        return this.avatar;
+    }
+    this.setAvatar = function(avatar) {
+        this.avatar = avatar;
+    }
+
+    this.getPoints = function() {
+        return this.points;
+    }
+    this.setPoints = function(points) {
+        this.points = points;
+    }
+
+    this.getPercentageWin= function() {
+        return this.percentageWin;
+    }
+    this.setPercentageWin = function(percentageWin) {
+        this.percentageWin = percentageWin;
+    }
+
+    this.getReady = function() {
+        return this.ready;
+    }
+    this.setReady = function(ready) {
+        this.ready = ready;
+    }
+
+    this.getPosition = function() {
+        return this.position;
+    }
+    this.setPosition = function(position) {
+        this.position = position;
+    }
+
+}
+
+module.exports = Player;
--- models/Room.js
+++ models/Room.js
@@ -1,17 +1,3 @@
-// var mongoose = require('mongoose');
-
-// var Schema = mongoose.Schema;
-
-// var userModel = new Schema({
-//     name:       {type: String},
-//     email:      {type: String, required:true},
-//     username:   {type: String, required:true},
-//     password:   {type: String, required:true}
-// });
-
-// module.exports = mongoose.model('Users',userModel);
-
-
 function Room(id){
     this.id = id;
     this.name = "Sala " + this.id;
