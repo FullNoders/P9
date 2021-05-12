efe3071f00d70aa4c6f619b78e1c28dd079afa70
euoc <ebresba@uoc.edu>
2021-05-12 23:39:33 +0200

    canvas render animation movement

--- public/javascripts/roomFunctions.js
+++ public/javascripts/roomFunctions.js
@@ -1,6 +1,8 @@
 
 function conquer(cell){
     // Animación canvas
+    renderCanvasP9(cell);
+/*     
     var canvas = document.createElement('canvas');
     canvas.width=64;
     canvas.height=64;
@@ -9,7 +11,7 @@ function conquer(cell){
     ctx.rect(0, 0, 64, 64);
     ctx.fillStyle = "black";
     ctx.fill();
-    document.getElementById(cell).appendChild(canvas);
+    document.getElementById(cell).appendChild(canvas); */
     // petición xhr
     setTimeout(function () {
         let rowcol = cell.split("-");
@@ -55,3 +57,118 @@ socket.on('next', function(msg) {
     alert("Un jugador ha abandonado la partida. El juego se dará por finalizado.");
     window.location.href = "/";
 }); */
+
+
+function renderCanvasP9(cell){
+    
+//set the variables
+var a = document.createElement('canvas'),
+c = a.getContext('2d'),
+w = a.width = 64,
+h = a.height = 64,
+area = w * h,
+particleNum = 300,
+ANIMATION;
+
+document.getElementById(cell).appendChild(a);
+
+var particles = [];
+
+
+//create the particles
+function Particle(i) {
+this.id = i;
+this.hue =  rand(50, 0, 1);
+this.active = false;
+}
+
+Particle.prototype.build = function() {
+this.x = w / 2;
+this.y = h / 2;
+this.r = rand(7, 2, 1);
+this.vx = Math.random() * 10 - 5;
+this.vy = Math.random() * 10 - 5;
+this.gravity = .01;
+this.opacity = Math.random() + .5;
+this.active = true;
+
+c.beginPath();
+  c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+c.fillStyle = "hsla(" + this.hue + ",100%,50%,1)";
+c.fill();
+};
+
+Particle.prototype.draw = function() {
+this.active = true;
+this.x += this.vx;
+this.y += this.vy;
+this.vy += this.gravity;
+this.hue -= 0.5;
+this.r = Math.abs(this.r - .05);
+
+c.beginPath();
+  c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+c.fillStyle = "hsla(" + this.hue + ",100%,50%,1)";
+c.fill();
+
+// reset particle
+if(this.r <= .05) {
+this.active = false;
+}
+};
+
+
+//functionality
+function drawScene() {
+c.fillStyle = "black";
+c.fillRect(0,0,w,h);
+
+for(var i = 0; i < particles.length; i++) {
+if(particles[i].active === true) {
+  particles[i].draw();
+} else {
+  particles[i].build();
+}
+}
+
+  ANIMATION = requestAnimationFrame(drawScene);
+}
+
+function initCanvas() {
+var s = getComputedStyle(a);
+
+if(particles.length) {
+particles = [];
+cancelAnimationFrame(ANIMATION);
+ANIMATION;
+console.log(ANIMATION);
+}
+
+w = a.width = 64;
+h = a.height = 64;
+
+for(var i = 0; i < particleNum; i++) {
+particles.push(new Particle(i));
+}
+
+drawScene();
+console.log(ANIMATION);
+}
+
+
+//init
+(function() {
+initCanvas();
+addEventListener('resize', initCanvas, false);
+})();
+
+
+//helper functions
+function rand(max, min, _int) {
+var max = (max === 0 || max)?max:1, 
+  min = min || 0, 
+  gen = min + (max - min) * Math.random();
+
+return (_int) ? Math.round(gen) : gen;
+};
+}
